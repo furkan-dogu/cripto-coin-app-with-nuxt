19a6ce00bbebf87dd19c31922dea88f262c13bdb
Furkan Doğu <furkandogu2018@gmail.com>
2024-09-01 13:55:34 +0300

    search ayarlanıyor

--- components/search.vue
+++ components/search.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
-search
-    </div>
+    <form class="w-full flex justify-center gap-2.5 my-10">
+        <input type="text" placeholder="Search for a coin" class="border w-1/2 shadow-sm rounded-md py-2 px-4">
+        <button type="submit" class="border border-blue-500">Search</button>
+    </form>
 </template>
 
 <script setup>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,32 @@
 <template>
-    <div>
+    <div class="container mx-auto flex flex-col items-center">
+        <h1 class="text-center text-4xl font-bold my-1">Cripto Coin App</h1>
         <Search />
-        <Table />
+        <Table :coins="coins" />
     </div>
 </template>
 
 <script setup>
+import axios from 'axios'
+const coins = ref([])
+
+const API_KEY = process.env.NUXT_API_KEY
+
+const getCoins = async () => {
+    const options = {
+        headers: { "x-access-token": API_KEY },
+    };
+
+    try {
+        const { data } = await axios.get("https://api.coinranking.com/v2/coins/", options)
+        coins.value = data.data.coins
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+onMounted(() => {
+    getCoins()
+})
 
 </script>
