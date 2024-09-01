73d376c4e44cdfd43cd2ac90bd5e930eef188880
Furkan Doğu <furkandogu2018@gmail.com>
2024-09-01 17:41:05 +0300

    table tamamlandı

--- components/search.vue
+++ components/search.vue
@@ -1,7 +1,7 @@
 <template>
-    <form class="w-full flex justify-center gap-2.5 my-10">
-        <input type="text" placeholder="Search for a coin" class="border w-1/2 shadow-sm rounded-md py-2 px-4">
-        <button type="submit" class="border border-blue-500">Search</button>
+    <form class="w-full flex justify-center gap-2.5 my-10 px-2.5">
+        <input type="text" placeholder="Search for a coin" class="border w-full sm:w-1/2 shadow-sm rounded-md py-2 px-4">
+        <button type="submit" class="bg-blue-600 text-white rounded-md py-2 px-4">Search</button>
     </form>
 </template>
 
--- components/table.vue
+++ components/table.vue
@@ -1,9 +1,40 @@
 <template>
-    <div>
-table
+    <div class="w-full">
+        <table class="w-full text-[9px] sm:text-base">
+            <thead>
+                <tr class="text-left border-b">
+                    <th>Name</th>
+                    <th>Rank</th>
+                    <th>Symbol</th>
+                    <th>Icon</th>
+                    <th>Change</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="coin in props.coins" :key="coin.uuid" class="border-b font-bold">
+                    <td class="text-gray-500">{{ coin.name }}</td>
+                    <td :style="{ color: coin.color }">{{ coin.rank }}</td>
+                    <td :style="{ color: coin.color }">{{ coin.symbol }}</td>
+                    <td>
+                        <a :href="coin.coinrankingUrl" target="_blank">
+                            <img :src="coin.iconUrl" :alt="coin.name" class="w-4 h-4 sm:w-8 sm:h-8">
+                        </a>
+                    </td>
+                    <td :class='["h-full" ,coin.change < 0 ? "text-red-500" : "text-green-500"]'>
+                        <div class="flex items-center justify-start">
+                            <span v-if="coin.change < 0"><IconsDown /></span>
+                            <span v-else><IconsUp /></span>
+                            {{ coin.change }}
+                        </div>
+                    </td>
+                    <td :style="{ color: coin.color }">${{ Number(coin.price).toFixed(2) }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script setup>
-
+const props = defineProps(['coins'])
 </script>
